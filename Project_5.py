45044b51f4b7ff9c12bb80e7856f99db24da3b0d
Swarnava Mukherjee <75041273+swarnava-96@users.noreply.github.com>
2021-08-04 05:37:59 +0530

    Add files via upload

---
+++ Project_5.py
@@ -0,0 +1,7 @@
+# Password Generator
+
+import random
+passlen = int(input("enter the length of password: "))
+s="abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+p = "".join(random.sample(s,passlen ))
+print(p)
